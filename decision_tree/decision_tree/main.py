d47bf78a29d9472b24f7291932ab0660f807fae7
Brenton Cromwell <bcromwell@opentable.com>
2023-09-04 12:01:36 +0100

    added small comment

--- decision_tree/decision_tree/main.py
+++ decision_tree/decision_tree/main.py
@@ -3,6 +3,7 @@ from decision_tree.model import DecisionTree
 
 data = GetData(path="data/animals.csv").prepare()
 x_data, y_data = FeatureBuilding(data).normal_index_encoding()
+#forgot to only include columns with encode here. Not fully sure if i would do it inside feature builder or here in the main
 
 model = DecisionTree(max_depth=10, min_size=1)
 model.fit(x_data, y_data)
